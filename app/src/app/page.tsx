2a4037c91669a376b7004e50fbfa1610cc4457f2
Skye <skyecarroll42@gmail.com>
2025-03-01 21:31:59 +0000

    fix lint error

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,7 +2,7 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
-  const { user, error, isLoading } = useUser();
+  const { user /*, error, isLoading*/ } = useUser();
 
   // if (isLoading) return <div>Loading...</div>;
   // if (error) return <div>{error.message}</div>;
